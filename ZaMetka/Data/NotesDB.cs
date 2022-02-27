1ad6b9b44025521cdf67c87955d8361a3a2b522a
Michel <Michel@MICHAEL-DESKTOP>
2022-02-27 09:44:44 +0300

    Добавьте файлы проекта.

---
+++ ZaMetka/Data/NotesDB.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ZaMetka.Model;
+
+namespace ZaMetka.Data
+{
+    internal class NotesDB
+    {
+        readonly SQLiteAsyncConnection db;
+
+        public NotesDB(string connectionString)
+        {
+            db = new SQLiteAsyncConnection(connectionString);
+            db.CreateTableAsync<Note>().Wait();
+        }
+
+        public Task<List<Note>> GetNotesAsync()
+        {
+            return db.Table<Note>().ToListAsync();
+        }
+
+        public Task<Note> GetNoteAsync(int id)
+        {
+            return db.Table<Note>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveNoteAsync(Note note)
+        {
+            if (note.Id != 0)
+            {
+                return db.UpdateAsync(note);
+            }
+            else
+            {
+                return db.InsertAsync(note);
+            }
+
+        }
+        public Task<int> DeleteNoteAsync(Note note)
+        {
+            return db.DeleteAsync(note);
+        }
+
+    }
+}
---
+++ ZaMetka/Model/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace ZaMetka.Model
+{
+    internal class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string NoteText { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
